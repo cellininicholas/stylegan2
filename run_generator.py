780c99c3dbbcce504ed6fdb42317fb1f54e5fe8f
Nick Cellini <ncellini@google.com>
2020-08-24 18:20:46 +1000

    numpy!

--- run_generator.py
+++ run_generator.py
@@ -443,12 +443,13 @@ def _parse_npy_files(files):
 
     for f in files.split(','):
         print("NPY FILE: '" + f + "'")
-        try:
-            zs.append(np.load(files[f]))
-        except IOError as e:
-            print ("I/O error(%d): %s" % (e.errno, e.strerror))
-        except ValueError:
-            print("An IOError occurred")
+        zs.append(np.load(files[f]))
+        # try:
+        #     zs.append(np.load(files[f]))
+        # except IOError as e:
+        #     print ("I/O error(%d): %s" % (e.errno, e.strerror))
+        # except ValueError:
+        #     print("An IOError occurred")
 
     return zs
 
