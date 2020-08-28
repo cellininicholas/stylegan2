e3ca84d1744f4805f95995a09c87a806841a0a09
Byron Hallett <bhallett@google.com>
2020-08-28 12:29:20 +1000

    add server file to accept latent walk requests

---
+++ server.py
@@ -0,0 +1,50 @@
+import numpy as np
+import PIL.Image
+import dnnlib
+
+from flask import Flask, request
+from flask_cors import cross_origin
+from os import environ
+from glob import glob
+
+import run_generator
+
+app = Flask(__name__)
+
+network = environ.get('NETWORK', None)
+if network == None:
+    print("Set env var NETWORK to stylegan pkl")
+    exit()
+
+
+@app.route("/morph", methods=["POST"])
+@cross_origin()
+def morph():
+    image_1 = request.json['image1']
+    image_2 = request.json['image2']
+
+    projection_patt = 'projection-results/{}*.npy'
+    npy_1 = glob(projection_patt.format(image_1))
+    npy_2 = glob(projection_patt.format(image_2))
+
+    ws_1 = run_generator._parse_npy_files(npy_1)
+    ws_2 = run_generator._parse_npy_files(npy_2)
+
+    kwargs = {
+        'network_pkl': network,
+        'truncation_psi': 1.0,
+        'walk_type': 'line-w',
+        'frames': 32,
+        'npys': [ws_1, ws_2],
+        'npys_type': 'w',
+        'result_dir': 'results'
+    }
+
+    sc = dnnlib.SubmitConfig()
+    sc.num_gpus = 1
+    sc.submit_target = dnnlib.SubmitTarget.LOCAL
+    sc.local.do_not_copy_source_files = True
+    sc.run_dir_root = kwargs.pop('result_dir')
+    sc.run_desc = "serve-latent-walk"
+    dnnlib.submit_run(
+        sc, 'run_generator.generate_latent_walk', **kwargs)
