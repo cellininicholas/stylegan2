7a98a2e853a254d7a5d40e416718a7db0c4b82c3
Byron Hallett <bhallett@google.com>
2020-10-01 16:22:32 +1000

    add seeded morph mode

---
+++ pre-compute.py
@@ -0,0 +1,2 @@
+#!/bin/sh
+# pre-compute some morphs
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import dnnlib
 
 from flask import Flask, request, send_file
 from flask_cors import cross_origin
-from os import environ, makedirs, path
+from os import environ, makedirs, path, remove
 from subprocess import run
 from glob import glob
 from math import sqrt
@@ -26,6 +26,42 @@ if network == None:
     exit()
 
 
+def kwargs_from_projections(image_1, image_2, frames):
+    projection_patt = 'w_latents/{}*.npy'
+    npy_1 = glob(projection_patt.format(image_1))[0]
+    npy_2 = glob(projection_patt.format(image_2))[0]
+
+    ws_1 = run_generator._parse_npy_files(npy_1)
+    ws_2 = run_generator._parse_npy_files(npy_2)
+
+    return {
+        'network_pkl': network,
+        'truncation_psi': 1.0,
+        'walk_type': 'line-w',
+        'frames': frames-1,
+        'npys': [ws_1, ws_2],
+        'npys_type': 'w',
+        'result_dir': 'server_walk_results',
+        'seeds': [],
+        'save_vector': False
+    }
+
+
+def kwards_from_seeds(image_1: str, image_2, frames):
+    seed1 = image_1.split("seed")[1].split(".png")[0]
+    seed2 = image_2.split("seed")[1].split(".png")[0]
+
+    return {
+        'network_pkl': network,
+        'truncation_psi': 1.0,
+        'walk_type': 'line',
+        'frames': frames-1,
+        'result_dir': 'server_walk_results',
+        'seeds': [seed1, seed2],
+        'save_vector': False
+    }
+
+
 @app.route("/stylegan/morph", methods=["POST"])
 @cross_origin()
 def morph():
@@ -33,6 +69,7 @@ def morph():
     image_2 = request.json['image2']
     fc = request.json['frame_count']
     no_cache = 'no_cache' in request.json and request.json['no_cache'] is True
+    seeded = 'seeded' in request.json and request.json['seeded'] is False
     if fc is None:
         fc = 9
     # floor the resolution
@@ -46,27 +83,13 @@ def morph():
     if not no_cache and bucket_blob.exists():
         return bucket_blob.public_url
 
-    try:
-        projection_patt = 'w_latents/{}*.npy'
-        npy_1 = glob(projection_patt.format(image_1))[0]
-        npy_2 = glob(projection_patt.format(image_2))[0]
-    except IndexError:
-        return "projection for one or both images not found", 400
-
-    ws_1 = run_generator._parse_npy_files(npy_1)
-    ws_2 = run_generator._parse_npy_files(npy_2)
-
-    kwargs = {
-        'network_pkl': network,
-        'truncation_psi': 1.0,
-        'walk_type': 'line-w',
-        'frames': frames-1,
-        'npys': [ws_1, ws_2],
-        'npys_type': 'w',
-        'result_dir': 'server_walk_results',
-        'seeds': [],
-        'save_vector': False
-    }
+    if seeded:
+        kwargs = kwards_from_seeds()
+    else:
+        try:
+            kwargs = kwargs_from_projections()
+        except IndexError:
+            return "projection for one or both images not found", 400
 
     sc = dnnlib.SubmitConfig()
     sc.num_gpus = 1
@@ -77,17 +100,17 @@ def morph():
     result_path, _ = dnnlib.submit_run(
         sc, 'run_generator.generate_latent_walk', **kwargs)
     morph_pattern = "{}/*.png".format(result_path)
-    sheet = make_spritesheet(morph_pattern, output_path, no_cache)
+    sheet = make_spritesheet(morph_pattern, output_path)
     # delete result_path since we have our sheet
     rmtree(result_path)
     # upload to bucket instead of storing locally
     bucket_blob.upload_from_filename(sheet)
-    rmtree(sheet)
+    remove(sheet)
     # return the file
     return bucket_blob.public_url
 
 
-def make_spritesheet(pattern, output_path, no_cache=False):
+def make_spritesheet(pattern, output_path):
     # make sure output dir exists
     makedirs("spritesheets", exist_ok=True)
     # count images
